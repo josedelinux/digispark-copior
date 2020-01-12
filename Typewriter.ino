98e40f1674fbe3b6361b80eb61d121539b4e4c33
josedelinux <37724086+josedelinux@users.noreply.github.com>
2020-01-12 12:51:03 +0800

    Add files via upload

---
+++ Typewriter.ino
@@ -0,0 +1,20 @@
+#include "DigiKeyboard.h"
+  //writen  by Josedelinux
+void setup() { 
+    //don`t need to set anything up
+}
+void loop() {
+
+
+  // this is generally not necessary but with some older systems it seems to
+  // prevent missing the first character after a delay:
+  DigiKeyboard.sendKeyStroke(0);
+
+  
+  //DigiKeyboard.delay(400); 
+  
+  DigiKeyboard.sendKeyStroke(KEY_V, MOD_CONTROL_LEFT);
+  DigiKeyboard.println();
+  //DigiKeyboard.sendKeyStroke(KEY_C, MOD_CONTROL_LEFT);
+}
+
